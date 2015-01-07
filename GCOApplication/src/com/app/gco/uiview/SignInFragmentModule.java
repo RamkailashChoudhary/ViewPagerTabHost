375584ca7b7fd198511c8ce072178b960cc963b7
RamkailashChoudhary <rbramkailash@gmail.com>
2015-01-08 00:30:33 +0530

    update

--- GCOApplication/src/com/app/gco/uiview/SignInFragmentModule.java
+++ GCOApplication/src/com/app/gco/uiview/SignInFragmentModule.java
@@ -2,6 +2,11 @@ package com.app.gco.uiview;
 
 import com.app.gco.HomeBaseActivity;
 import com.app.gco.R;
+import com.app.gco.dao.LoginData;
+import com.app.gco.delegates.ServerAPI;
+import com.app.gco.utils.Utils;
+import com.app.gco.webrequest.RequestDataModule;
+import com.loopj.android.http.RequestParams;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +25,7 @@ public class SignInFragmentModule extends Fragment implements OnClickListener{
 	private EditText emailTxt,userNameTxt,passwordTxt;
 	private OnClickListener parentLayoutListener,forgotPasswordListener;
 	private TextView forgotPasswordTxt;
+	private RequestDataModule mRequestDataModule;
 	
 	public static SignInFragmentModule newInstance(OnClickListener clickListener,OnClickListener forgotpasswordListener){
 		
@@ -60,9 +66,34 @@ public class SignInFragmentModule extends Fragment implements OnClickListener{
 
 		if(v == signInsignUpButton){
 			
-		  //web service signin	
-			Intent menuBoard = new Intent(getActivity(),HomeBaseActivity.class);
-			startActivity(menuBoard);
+		  if(emailTxt.getText().toString().length() > 0){
+			
+			  if(passwordTxt.getText().toString().length() > 0){
+	
+				  if(Utils.isValidEmailAddress(emailTxt.getText().toString()))
+					  loginServerRequest();
+				  else
+					  Utils.showToast(getActivity(), "Please enter correct email format");
+			  }else
+				  Utils.showToast(getActivity(), "Please enter password");
+		  }else
+			  Utils.showToast(getActivity(), "Please enter email");
+	//		Intent menuBoard = new Intent(getActivity(),HomeBaseActivity.class);
+		//	startActivity(menuBoard);
 		}
+	}
+	
+	private void loginServerRequest(){
+		
+		mRequestDataModule = new RequestDataModule(getActivity());
+		mRequestDataModule.setUrl(ServerAPI.LOGIN);
+		mRequestDataModule.setRequestTag(ServerAPI.LOGIN_TAG);
+		mRequestDataModule.setMethodType(ServerAPI.POST_METHOD);
+		RequestParams params = new RequestParams();
+		params.put("email", emailTxt.getText().toString());
+		params.put("password", passwordTxt.getText().toString());
+		mRequestDataModule.setParams(params);
+		mRequestDataModule.setClassType(LoginData.class);
+		mRequestDataModule.execute();
 	}
 }
