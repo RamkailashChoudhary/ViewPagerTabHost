4e1e1b600851e0c230202d4224ca53cebb3db122
admin <rbramkailash@gmail.com>
2015-01-07 16:11:36 +0530

    updated

--- GCOApplication/src/com/app/gco/HomeBaseActivity.java
+++ GCOApplication/src/com/app/gco/HomeBaseActivity.java
@@ -2,20 +2,15 @@ package com.app.gco;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Window;
-import android.widget.TabHost;
+import android.widget.Toast;
 
-import com.app.gco.tabbar.HomeTabAdapter;
-import com.app.gco.uiview.HomeFragmentModule;
-import com.app.gco.uiview.InfoFragmentModule;
+import com.app.gco.adapters.HomeTabAdapter;
+import com.viewpagerindicator.TabPageIndicator;
 
 public class HomeBaseActivity extends FragmentActivity {
-
-	private TabHost mTabHost;
-	private ViewPager mViewPager;
-	private HomeTabAdapter mHomeTabAdapter;
-//	After uploaded Github data
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +18,13 @@ public class HomeBaseActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_activity);
 
-		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-		mTabHost.setup();
+		  FragmentPagerAdapter adapter = new  HomeTabAdapter(getSupportFragmentManager());
 
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mHomeTabAdapter = new HomeTabAdapter(this, mTabHost, mViewPager);
+	        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+	        pager.setAdapter(adapter);
 
-		mHomeTabAdapter.addTab(mTabHost.newTabSpec("one").setIndicator("One"), HomeFragmentModule.class, savedInstanceState);
-		mHomeTabAdapter.addTab(mTabHost.newTabSpec("two").setIndicator("Two"), InfoFragmentModule.class, savedInstanceState);
-		mHomeTabAdapter.addTab(mTabHost.newTabSpec("Three").setIndicator("Three"), InfoFragmentModule.class, savedInstanceState);
-
-		if (savedInstanceState != null) {
-			
-			mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
-		}
-
+	        Toast.makeText(HomeBaseActivity.this, "Default Tab", Toast.LENGTH_SHORT).show();
+	        TabPageIndicator indicator = (TabPageIndicator)findViewById(R.id.indicator);
+	        indicator.setViewPager(pager);
 	}
 }
